5103aac1781d09ed9ddd41b2f7aa2036cf5935d2
PJungeun <48914872+PJungeun@users.noreply.github.com>
2021-05-08 23:41:25 +0900

    Update models.py

--- CamApp/models.py
+++ CamApp/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Menu(models.Model):
     menu_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, unique=True)
-    image = models.TextField()
+    image = models.ImageField(blank=True, upload_to="image", null=True)
     des = models.TextField()
     price = models.CharField(max_length=15)
     cat = models.CharField(max_length=30)
@@ -15,11 +15,15 @@ class Menu(models.Model):
         managed = False
         db_table = 'menu'
 
-
 class Order(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    time = models.DateTimeField()
-    total_price = models.CharField(max_length=15)
+    order_id = models.AutoField(primary_key=True,)
+    time = models.DateTimeField(auto_now_add=True)
+    total_price = models.IntegerField()
+
+    Myordernum=1  #####주문서번호 (일단은 로그인시 1로설정해놓음 나중에 가장최근값을 가져오도록)
+
+    def auto(ordernum):
+     Order.Myordernum=ordernum+1
 
     class Meta:
         managed = False
@@ -28,11 +32,13 @@ class Order(models.Model):
 
 class OrderList(models.Model):
     list_id = models.AutoField(primary_key=True)
-    order_num = models.ForeignKey(Order, models.DO_NOTHING, db_column='order_num')
-    menu_num = models.ForeignKey(Menu, related_name='menu_num', on_delete=models.CASCADE, db_column='menu_num')
-    menu_name = models.ForeignKey(Menu, to_field='name', related_name='menu_name', on_delete=models.CASCADE, db_column='menu_name')
-    quatity = models.CharField(max_length=3)
-
+    order_num = models.ForeignKey(Order, on_delete=models.DO_NOTHING, db_column='order_num')
+    menu_num = models.ForeignKey(Menu, to_field='menu_id',related_name='menu_num', on_delete=models.CASCADE, db_column='menu_num')
+    menu_name = models.ForeignKey(Menu,to_field='name', related_name='menu_name', on_delete=models.CASCADE, db_column='menu_name')
+    quantity = models.IntegerField(max_length=3)
+    price = models.IntegerField(max_length=3)
+    cup=models.CharField(max_length=15)
+ 
     class Meta:
         managed = False
         db_table = 'order_list'
